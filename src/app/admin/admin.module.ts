5d1c9fcac69666fa2c55c16f62e9a884d8d72948
eamre <emrealkan.1@outlook.com>
2023-09-17 18:51:17 +0300

    bootstrap

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LayoutModule } from './layout/layout.module';
+import { ComponentsModule } from './components/components.module';
+import { LayoutComponent } from './layout/layout.component';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LayoutModule,
+    ComponentsModule
+  ],
+  exports:[
+    LayoutModule
+  ]
+})
+export class AdminModule { }
