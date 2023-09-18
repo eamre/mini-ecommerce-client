fb6ccf8b4d8e41b78edb314af6fd87fdaf458f5c
eamre <emrealkan.1@outlook.com>
2023-09-18 17:57:54 +0300

    ders14

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertifyService, MessagePosition, MessageType } from 'src/app/services/admin/alertify.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private alertify: AlertifyService) { }
 
   ngOnInit(): void {
   }
 
+  m(){
+    this.alertify.message("merhaba",{messageType:MessageType.Success,delay:5,
+      dismissOthers:false,position:MessagePosition.BottomRight});
+  }
+  d(){
+    this.alertify.dismiss();
+  }
 }
---
+++ src/app/services/admin/alertify.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+declare var alertify: any;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertifyService {
+
+  constructor() { }
+  // message(message:string, messageType: MessageType,
+  //   position: MessagePosition, delay: number=3, dismissOthers: boolean=false){
+  message(message: string, options: Partial<AlertifyOptions>){
+    alertify.set('notifier','delay', options.delay);
+    alertify.set('notifier','position', options.position)
+    const msg = alertify[options.messageType](message);
+    if(options.dismissOthers){
+      msg.dismissOthers();
+    }
+  }
+  dismiss(){
+    alertify.dismissAll();
+  }
+}
+
+export class AlertifyOptions{
+  messageType: MessageType = MessageType.Message;
+  position: MessagePosition = MessagePosition.BottomRight;
+  delay: number=3;
+  dismissOthers: boolean=false;
+}
+
+export enum MessageType{
+  Error="error",
+  Message="message",
+  Notify="notify",
+  Success="success",
+  Warning="warning"
+}
+
+export enum MessagePosition{
+  TopLeft="top-left",
+  TopRight="top-right",
+  TopCenter="top-center",
+  BottomLeft="bottom-left",
+  BottomRight="bottom-right",
+  BottomCenter="bottom-center"
+}
