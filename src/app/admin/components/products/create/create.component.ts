321eb6ea0e098629bf421d9c22984a382846be52
eamre <emrealkan.1@outlook.com>
2023-09-22 22:17:41 +0300

    ders 20

--- src/app/admin/components/products/create/create.component.ts
+++ src/app/admin/components/products/create/create.component.ts
@@ -2,34 +2,49 @@ import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { CreateProduct } from 'src/app/contracts/create_product';
-import { AlertifyService, MessagePosition, MessageType } from 'src/app/services/admin/alertify.service';
+import {
+  AlertifyService,
+  MessagePosition,
+  MessageType,
+} from 'src/app/services/admin/alertify.service';
 import { ProductService } from 'src/app/services/common/models/product.service';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
+  styleUrls: ['./create.component.scss'],
 })
 export class CreateComponent extends BaseComponent implements OnInit {
-
-  constructor(private productService :ProductService,spinner: NgxSpinnerService, private alertify: AlertifyService) {
+  constructor(
+    private productService: ProductService,
+    spinner: NgxSpinnerService,
+    private alertify: AlertifyService
+  ) {
     super(spinner);
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  create(name:HTMLInputElement, stock:HTMLInputElement, price:HTMLInputElement){
+  create(
+    name: HTMLInputElement,
+    stock: HTMLInputElement,
+    price: HTMLInputElement
+  ) {
     this.showSpinner(SpinnerType.BallAtom);
     const create_product: CreateProduct = new CreateProduct();
     create_product.name = name.value;
     create_product.stock = parseInt(stock.value);
     create_product.price = parseFloat(price.value);
 
-    this.productService.create(create_product,()=>{
+    this.productService.create(create_product, () => {
       this.hideSpinner(SpinnerType.BallAtom),
-      this.alertify.message("ürün başarıyla eklendi",
-      {messageType: MessageType.Success, dismissOthers:true, position:MessagePosition.TopRight})});
+        this.alertify.message('ürün başarıyla eklendi', {
+          messageType: MessageType.Success,
+          dismissOthers: true,
+          position: MessagePosition.TopRight,
+        });
+    },(errorMessage)=>{
+      this.alertify.message(errorMessage,{messageType:MessageType.Error,position:MessagePosition.TopRight});
+    });
   }
-
 }
--- src/app/services/common/models/product.service.ts
+++ src/app/services/common/models/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClientService } from '../http-client.service';
 import { CreateProduct } from 'src/app/contracts/create_product';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -8,11 +9,23 @@ import { CreateProduct } from 'src/app/contracts/create_product';
 export class ProductService {
   constructor(private httpClientService: HttpClientService) {}
 
-  create(product: CreateProduct, successCallBack?: any) {
+  create(product: CreateProduct, successCallBack?: any, errorCallBack?:(errorMessage:string)=>void) {
     this.httpClientService
       .post({ controller: 'products' }, product)
-      .subscribe((result) => {
-        successCallBack();
+      .subscribe({
+        next: (result) => {
+          successCallBack();
+        },
+        error: (errorResponse:HttpErrorResponse)=>{
+          const _error:Array<{key:string, value:Array<string>}> = errorResponse.error;
+          let message = "";
+          _error.forEach((v,index)=>{
+            v.value.forEach((_v, _index)=>{
+              message += `${_v} <br>`
+            });
+          });
+          errorCallBack(message);
+        }
       });
   }
 }
