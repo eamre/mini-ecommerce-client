edcf9b6f014ac94aa6ca434f889e9b0d0d12ad52
eamre <emrealkan.1@outlook.com>
2023-09-22 19:37:59 +0300

    ders19

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
-import { Product } from 'src/app/contracts/product';
+import { CreateProduct } from 'src/app/contracts/create_product';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 @Component({
@@ -18,56 +18,6 @@ export class ProductsComponent extends BaseComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.showSpinner(SpinnerType.BallAtom);
-
-    this.httpClientService
-      .get<Product[]>({
-        controller: 'products',
-      })
-      .subscribe((data) => console.log(data));
-
-    // this.httpClientService
-    //   .get({
-    //     fullEndPoint: 'https://jsonplaceholder.typicode.com/users',
-    //   })
-    //   .subscribe((data:any) => {
-    //     for(let user of data){
-    //       console.log(user.name);
-    //     }
-    //   });
-
-    // this.httpClientService.post(
-    //   {
-    //     controller: 'products',
-    //   },
-    //   {
-    //     name: 'product2',
-    //     stock: 200,
-    //     price: 25,
-    //   }
-    // ).subscribe();
-
-    // this.httpClientService
-    //   .put(
-    //     {
-    //       controller: 'products',
-    //     },
-    //     {
-    //       id: '16497eb3-a452-4222-a3ce-66fa115d308f',
-    //       name: 'renkli kagit',
-    //       stock: 20,
-    //       price: 28,
-    //     }
-    //   )
-    //   .subscribe();
-
-    // this.httpClientService
-    //   .delete(
-    //     {
-    //       controller: 'products',
-    //     },
-    //     '16497eb3-a452-4222-a3ce-66fa115d308f'
-    //   )
-    //   .subscribe();
+    
   }
 }
