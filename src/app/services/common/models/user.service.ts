cea0cce5bcf79f25929287538e855abd6d2fbb11
eamre <emrealkan.1@outlook.com>
2023-11-07 23:12:15 +0300

    ders40

--- src/app/services/common/models/user.service.ts
+++ src/app/services/common/models/user.service.ts
@@ -3,13 +3,17 @@ import { HttpClientService } from '../http-client.service';
 import { User } from 'src/app/entities/user';
 import { CreateUserResponse } from 'src/app/contracts/users/create_user';
 import { Observable, firstValueFrom } from 'rxjs';
+import { Token } from 'src/app/contracts/token/token';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../../ui/custom-toastr.service';
+import { TokenResponse } from 'src/app/contracts/token/tokenResponse';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private httpClientService: HttpClientService) {
+  constructor(private httpClientService: HttpClientService,
+    private toastrService : CustomToastrService) {
   }
 
   async create(user:User):Promise<CreateUserResponse>{
@@ -20,14 +24,23 @@ export class UserService {
     return await firstValueFrom(createUserObservable) as CreateUserResponse;
   }
 
-  async login(usernameOrEmail: string, password:string, cb?:()=>void):Promise<void>{
-    const loginUserObservable:Observable<any> = this.httpClientService.post({
+  async login(usernameOrEmail: string, password:string, cb?:()=>void):Promise<any>{
+    const loginUserObservable:Observable<any | TokenResponse> = this.httpClientService.post<any | TokenResponse>({
       controller:"users",
       action:"login"
     },{usernameOrEmail,password})
 
-    return await firstValueFrom(loginUserObservable)
+    const tokenReponse: TokenResponse = await firstValueFrom(loginUserObservable) as TokenResponse;
+
     cb();
+    if(tokenReponse){
+      localStorage.setItem("accessToken",tokenReponse.token.accessToken);
+
+      this.toastrService.message("Kullanıcı girişi başariyla sağlandi","Giriş başarili",{
+        messageType:ToastrMessageType.Success,
+        position:ToastrPosition.TopRight
+      })
+    }
   }
 
 }
